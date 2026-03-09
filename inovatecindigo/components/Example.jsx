228b4f91bc11cda39949ae9e8697a4287250f655
Evaldo Klock <neto.nemesis@gmail.com>
2026-03-09 19:39:02 -0400

    Add Indigo theme templates and initial setup
    - Created new Mako templates for authenticated and non-authenticated navigation bars.
    - Added logo header template with enterprise support.
    - Implemented user dropdown for authenticated users.
    - Developed index and overlay templates for the landing page.
    - Introduced static templates for About, Blog, Contact, Donate, Help, Privacy Policy, and Terms of Service pages.
    - Added theme toggle button for dark/light mode switching.
    - Updated learner dashboard program card and programs fragment templates.
    - Created an initialization script for theme assignment in the LMS.
    - Added pyproject.toml for project metadata and dependencies.
    - Included a screenshot of the landing page.

---
+++ inovatecindigo/components/Example.jsx
@@ -0,0 +1,4 @@
+
+// Add your imports in inovatecindigo/components/imports.jsx.
+// Add your component in inovatecindigo/components/Example.jsx.
+// Declare your component in inovatecindigo/patches/mfe-env-config-runtime-definitions e.g. {{- patch("Example.jsx") }}.
---
+++ inovatecindigo/plugin.py
@@ -0,0 +1,349 @@
+from __future__ import annotations
+
+import itertools
+import json
+import os
+import typing as t
+from glob import glob
+
+import importlib_resources
+from tutor import hooks
+from tutor.__about__ import __version_suffix__
+from tutormfe.hooks import MFE_APPS, MFE_ATTRS_TYPE, PLUGIN_SLOTS
+
+from .__about__ import __version__
+
+# Handle version suffix in main mode, just like tutor core
+if __version_suffix__:
+    __version__ += "-" + __version_suffix__
+
+
+################# Configuration
+config: t.Dict[str, t.Dict[str, t.Any]] = {
+    # Add here your new settings
+    "defaults": {
+        "VERSION": __version__,
+        "WELCOME_MESSAGE": "The place for all your online learning",
+        "PRIMARY_COLOR": "#244FFF",  # Inovatec
+        "ENABLE_DARK_TOGGLE": True,
+        # Footer links are dictionaries with a "title" and "url"
+        # To remove all links, run:
+        # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
+        "FOOTER_NAV_LINKS": [
+            {"title": "About Us", "url": "/about"},
+            {"title": "Blog", "url": "/blog"},
+            {"title": "Donate", "url": "/donate"},
+            {"title": "Terms of Service", "url": "/tos"},
+            {"title": "Privacy Policy", "url": "/privacy"},
+            {"title": "Help", "url": "/help"},
+            {"title": "Contact Us", "url": "/contact"},
+        ],
+    },
+    "unique": {},
+    "overrides": {},
+}
+
+# Theme templates
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    str(importlib_resources.files("inovatecindigo") / "templates")
+)
+# This is where the theme is rendered in the openedx build directory
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("indigo", "build/openedx/themes"),
+        ("indigo/env.config.jsx", "plugins/mfe/build/mfe"),
+    ],
+)
+
+# Force the rendering of scss files, even though they are included in a
+# "partials" directory
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_items(
+    [
+        r"indigo/lms/static/sass/partials/lms/theme/",
+        r"indigo/cms/static/sass/partials/cms/theme/",
+    ]
+)
+
+
+# init script: set theme automatically
+with open(
+    os.path.join(
+        str(importlib_resources.files("inovatecindigo") / "templates"),
+        "indigo",
+        "tasks",
+        "init.sh",
+    ),
+    encoding="utf-8",
+) as task_file:
+    hooks.Filters.CLI_DO_INIT_TASKS.add_item(("lms", task_file.read()))
+
+
+# Override openedx & mfe docker image names
+@hooks.Filters.CONFIG_DEFAULTS.add(priority=hooks.priorities.LOW)
+def _override_openedx_docker_image(
+    items: list[tuple[str, t.Any]],
+) -> list[tuple[str, t.Any]]:
+    openedx_image = ""
+    mfe_image = ""
+    for k, v in items:
+        if k == "DOCKER_IMAGE_OPENEDX":
+            openedx_image = v
+        elif k == "MFE_DOCKER_IMAGE":
+            mfe_image = v
+    if openedx_image:
+        items.append(("DOCKER_IMAGE_OPENEDX", f"{openedx_image}-inovatec-indigo"))
+    if mfe_image:
+        items.append(("MFE_DOCKER_IMAGE", f"{mfe_image}-inovatec-indigo"))
+    return items
+
+
+# Load all configuration entries
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [(f"INDIGO_{key}", value) for key, value in config["defaults"].items()]
+)
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [(f"INDIGO_{key}", value) for key, value in config["unique"].items()]
+)
+hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
+
+
+#  MFEs that are styled using Indigo
+indigo_styled_mfes = [
+    "learning",
+    "learner-dashboard",
+    "profile",
+    "account",
+    "discussions",
+]
+
+for mfe in indigo_styled_mfes:
+    hooks.Filters.ENV_PATCHES.add_items(
+        [
+            (
+                f"mfe-dockerfile-post-npm-install-{mfe}",
+                """
+RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.0'
+""",  # noqa: E501
+            ),
+        ]
+    )
+
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-dockerfile-post-npm-install-authn",
+        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.0'",
+    )
+)
+
+# Include js file in lms main.html, main_django.html, and certificate.html
+
+hooks.Filters.ENV_PATCHES.add_items(
+    [
+        # for production
+        (
+            "openedx-common-assets-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['indigo/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+""",
+        ),
+        # for development
+        (
+            "openedx-lms-development-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['indigo/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+
+MFE_CONFIG['INDIGO_ENABLE_DARK_TOGGLE'] = {{ INDIGO_ENABLE_DARK_TOGGLE }}
+MFE_CONFIG['INDIGO_FOOTER_NAV_LINKS'] = {{ INDIGO_FOOTER_NAV_LINKS }}
+""",
+        ),
+        (
+            "openedx-lms-production-settings",
+            """
+MFE_CONFIG['INDIGO_ENABLE_DARK_TOGGLE'] = {{ INDIGO_ENABLE_DARK_TOGGLE }}
+MFE_CONFIG['INDIGO_FOOTER_NAV_LINKS'] = {{ INDIGO_FOOTER_NAV_LINKS }}
+""",
+        ),
+    ]
+)
+
+
+# Add react components and patches from tutor-indigo
+for path in itertools.chain(
+    glob(
+        os.path.join(str(importlib_resources.files("inovatecindigo") / "components"), "*")
+    ),
+    glob(os.path.join(str(importlib_resources.files("inovatecindigo") / "patches"), "*")),
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+for mfe in indigo_styled_mfes:
+    PLUGIN_SLOTS.add_item(
+        (
+            mfe,
+            "org.openedx.frontend.layout.footer.v1",
+            """
+            {
+                op: PLUGIN_OPERATIONS.Hide,
+                widgetId: 'default_contents',
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'indigo_footer',
+                    type: DIRECT_PLUGIN,
+                    priority: 1,
+                    RenderWidget: IndigoFooter,
+                },
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'read_theme_cookie',
+                    type: DIRECT_PLUGIN,
+                    priority: 2,
+                    RenderWidget: AddDarkTheme,
+                },
+            },
+  """,
+        ),
+    )
+    if mfe != "learning":
+        PLUGIN_SLOTS.add_item(
+            (
+                mfe,
+                "desktop_secondary_menu_slot",
+                """
+                {
+                    op: PLUGIN_OPERATIONS.Insert,
+                    widget: {
+                        id: 'theme_switch_button',
+                        type: DIRECT_PLUGIN,
+                        RenderWidget: ToggleThemeButton,
+                    },
+                },
+        """,
+            )
+        )
+        PLUGIN_SLOTS.add_items(
+            [
+                (
+                    # Hide the default mobile header as it only shows logo
+                    mfe,
+                    "mobile_header_slot",
+                    """
+                {
+                    op: PLUGIN_OPERATIONS.Hide,
+                    widgetId: 'default_contents',
+                }
+                """,
+                ),
+                (
+                    mfe,
+                    "mobile_header_slot",
+                    """
+                {
+                    op: PLUGIN_OPERATIONS.Insert,
+                    widget: {
+                        id: 'theme_switch_button',
+                        type: DIRECT_PLUGIN,
+                        RenderWidget: MobileViewHeader,
+                    },
+                },
+                """,
+                ),
+            ]
+        )
+
+PLUGIN_SLOTS.add_items(
+    [
+        (
+            # Hide the default Help Link added in plugin slot
+            "learning",
+            "learning_help_slot",
+            """
+        {
+            op: PLUGIN_OPERATIONS.Hide,
+            widgetId: 'default_contents',
+        }
+        """,
+        ),
+        (
+            "learning",
+            "learning_help_slot",
+            """
+        {
+            op: PLUGIN_OPERATIONS.Insert,
+            widget: {
+                id: 'theme_switch_button',
+                type: DIRECT_PLUGIN,
+                RenderWidget: ToggleThemeButton,
+            },
+        },
+        """,
+        ),
+    ]
+)
+
+paragon_theme_urls = {
+    "variants": {
+        "light": {
+            "urls": {
+                "default": "https://raw.githubusercontent.com/edly-io/brand-openedx/refs/heads/ulmo/indigo/dist/light.min.css",
+                "brandOverride": "https://raw.githubusercontent.com/edly-io/brand-openedx/refs/heads/ulmo/indigo/dist/light.min.css",
+            },
+        },
+        "dark": {
+            "urls": {
+                "default": "https://raw.githubusercontent.com/edly-io/brand-openedx/refs/heads/ulmo/indigo/dist/dark.min.css",
+                "brandOverride": "https://raw.githubusercontent.com/edly-io/brand-openedx/refs/heads/ulmo/indigo/dist/dark.min.css",
+            }
+        },
+    }
+}
+
+fstring = f"""
+MFE_CONFIG["PARAGON_THEME_URLS"] = {json.dumps(paragon_theme_urls)}
+"""
+
+hooks.Filters.ENV_PATCHES.add_item(("mfe-lms-common-settings", fstring))
+
+
+@MFE_APPS.add()  # type: ignore
+def _add_themed_logo(
+    mfes: dict[str, MFE_ATTRS_TYPE],
+) -> dict[str, MFE_ATTRS_TYPE]:
+    for mfe in mfes:
+        PLUGIN_SLOTS.add_item(
+            (
+                str(mfe),
+                "logo_slot",
+                """
+                {
+                    op: PLUGIN_OPERATIONS.Hide,
+                    widgetId: 'default_contents',
+                },
+                {
+                    op: PLUGIN_OPERATIONS.Insert,
+                    widget: {
+                        id: 'custom_logo',
+                        type: DIRECT_PLUGIN,
+                        RenderWidget: ThemedLogo,
+                    }
+                }
+            """,
+            )
+        )
+
+    return mfes
